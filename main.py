e83ae90a093868022c2e843aba41b594a043957a
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-20 18:23:18 +0300

    Added the MyContact class

--- main.py
+++ main.py
@@ -42,6 +42,66 @@ class Contact(Resource):
     write(all_)
     return {"status": "successful deleted all the data"}
 
+
+class MyContact(Resource):
+  """this will deal with individual forms of data"""
+  def get(self, cont_id):
+    """returns an individual for the id put"""
+    return {"id": cont_id, 
+            "data": all_[cont_id],
+            "status": "read successfully"
+           }
+
+
+  def post(self, cont_id):
+    """adds the new data item to the contact.json file"""
+    data = {
+      "name": request.form['name'],
+      "email": request.form['email'],
+      "contact": request.form['contact'],
+      "relationship": request.form['relationship']
+    }
+    all_[cont_id] = data
+    write(all_)
+    return {
+      "id": cont_id ,
+      "data": data,
+      "status":"created a new record successfully"
+    }
+
+  
+  def put(self, cont_id ):
+    """this will be responsible for updating the data"""
+    data = {
+      "name": request.form['name'],
+      "email": request.form['email'],
+      "contact": request.form['contact'],
+      "relationship": request.form['relationship']
+    }
+    all_[cont_id] = data
+    write(all_)
+    return {
+      "id": cont_id ,
+      "data": data,
+      "status":"updated a new record successfully"
+    }
+
+  
+  def delete(self, cont_id ):
+    """this will delete a single instance with the same name""" 
+    all_.pop(cont_id)
+    write(all_)
+    return {
+    "message": "successfully  deleted"
+    }
+
+"""this will deal with the routing of the urls"""
+api.add_resource(Hello, "/")
+api.add_resource(Contact, "/contact")
+api.add_resource(MyContact, "/contact/<int:cont_id>")
+
+
+
 if __name__ == "__main__":
   app.run(debug=True)
 
