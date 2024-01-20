a8cb8cbae75a29faef058b117699b999e63238c5
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-20 17:56:30 +0300

    Reading and writing to the json file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
+import json
 
 
 app = Flask(__name__)
@@ -13,4 +14,16 @@ class Hello(Resource):
       "greeting":"hello world"
     }
 
+def read():
+  """this will  read the data from the contact.json file"""
+  data = {}
+  with open("contact.json", "r") as f:
+    data = json.load(f)
+  return data
+
+
+def write(data):
+  """this will write the data to the json file"""
+  with open("contact.json", "w") as f:
+    json.dump(data)
 
